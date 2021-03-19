1f7491ffe77b30e7849229cb6698605ad2908f85
Kai Nguyen <52805377+rainsreign@users.noreply.github.com>
2021-03-18 21:17:55 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/sidenavtest/ThirdFragment.java
@@ -0,0 +1,18 @@
+package com.example.sidenavtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ThirdFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.nav_third_fragment, container, false);
+    }
+}
